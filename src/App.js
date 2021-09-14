668800eccaf1087c798db5e80cbeb68d2c4c2fd1
Mikey Cleworth <81911172+mikey-cle@users.noreply.github.com>
2021-09-14 15:25:44 +0100

    fixed nav and added a text section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/navbar.css';
+import Nav from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className = "body">
+        <Nav />
+        <div className= 'central'>
+          <div className= 'left'>
+              <h1>What we do</h1>
+              <p>The Moss Fairy Crew are a group of volunteers from the local area. We dedicate our own time to ensure that Collier's Moss remains a place of beauty and tranquility. We do this through frequent litter picks, where we remove any unsightly litter to keep this local beauty spot a place for all to enjoy, including the indigenous wildlife.</p>
+          </div>
+          <div className = 'right'>
+
+          </div>
+        </div>
+   </div>
+   
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return (
+    <div className='nav'>
+   <h1>The Moss Fairy Crew</h1>
+   <div className= 'menu'>
+   <ul>
+       <li>Home</li>
+       <li>Contact</li>
+       <li>About</li>
+   </ul>
+    </div></div>
+    )
+}
+export default Nav;
